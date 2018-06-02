a626b9bafd0d8178799ba950493946e99db0ee37
nicolalandro <nicolaxx94@live.it>
2018-06-02 10:00:01 +0200

    wip search train

--- app/src/main/java/com/example/mint/alarmclock/TrainInfoActivity.java
+++ app/src/main/java/com/example/mint/alarmclock/TrainInfoActivity.java
@@ -6,14 +6,33 @@ import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.DatePicker;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import com.example.mint.alarmclock.TrenitaliaAPI.Train;
+import com.example.mint.alarmclock.TrenitaliaAPI.TrenitaliaService;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class TrainInfoActivity extends AppCompatActivity {
     private String[] AUTOCOMPLETE = new String[]{
@@ -36,12 +55,22 @@ public class TrainInfoActivity extends AppCompatActivity {
 
     private TextView mTimeTextView;
     private TextView mDateTextView;
+    private ListView mTrainListView;
+    private ArrayAdapter<String> mTrainArrayAdapter;
+    private ArrayList<String> listItems = new ArrayList<>();
+    private TrenitaliaService mTrenitaliaService;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_train_info);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://www.viaggiatreno.it/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        mTrenitaliaService = retrofit.create(TrenitaliaService.class);
 
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, AUTOCOMPLETE);
 
@@ -50,13 +79,21 @@ public class TrainInfoActivity extends AppCompatActivity {
 
         mTimeTextView = findViewById(R.id.timeFromTextView);
         mDateTextView = findViewById(R.id.dateTextView);
+        mTrainListView = findViewById(R.id.trainListView);
+        mTrainArrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                listItems
+        );
+        mTrainListView.setAdapter(mTrainArrayAdapter);
 
         Calendar calendar = Calendar.getInstance();
         mHour = calendar.get(Calendar.HOUR_OF_DAY);
         mMinutes = calendar.get(Calendar.MINUTE);
         mYear = calendar.get(Calendar.YEAR);
         mMonth = calendar.get(Calendar.MONTH);
-        mYear = calendar.get(Calendar.DAY_OF_MONTH);
+//        mYear = calendar.get(Calendar.DAY_OF_MONTH);
+        mYear = 2018;
 
         mTimeTextView.setText(formatTime(mHour, mMinutes));
         mDateTextView.setText(formatDate(mYear, mMonth, mDayOfMonth));
@@ -97,6 +134,35 @@ public class TrainInfoActivity extends AppCompatActivity {
         returnIntent.putExtra("result", "data");
         setResult(Activity.RESULT_OK, returnIntent);
         finish();
+    }
+
+    public void OnSearchClicked(View view) {
+        String from = mFromAutocopleteView.getText().toString().split("|")[1];
+        Observable<String> delayObsevable = Observable.just(from);
+        delayObsevable
+                .subscribeOn(Schedulers.newThread())
+                .subscribe(
+                        new Consumer<String>() {
+                            @Override
+                            public void accept(String s) throws Exception {
+                                DateFormat formatter = new SimpleDateFormat("EEE MMM dd yyyy HH:mm:ss 'GMT'Z (z)", Locale.US);
+                                Date today = Calendar.getInstance().getTime();
+                                String reportDate = formatter.format(today);
+                                Call<List<Train>> call = mTrenitaliaService.searchTrain("S01031", reportDate);
+                                Response<List<Train>> response = call.execute();
+                                List<Train> result = response.body();
+                                Log.d("DEBUG!!!", "accept: "+result.size());
+//                                setTrainList(result);
+                            }
+                        }
+                );
+    }
+
+    public void setTrainList(List<Train> trains){
+        mTrainArrayAdapter.clear();
+        for (Train train: trains) {
+            mTrainArrayAdapter.add(train.toString());
+        }
     }
 
     private String formatTime(int hour, int minutes) {
